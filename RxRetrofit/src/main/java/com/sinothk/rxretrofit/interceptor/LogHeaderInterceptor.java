7950b8956d5aaec748edc807d110e58e419f1b65
LiangYT <381518188@qq.com>
2019-03-26 17:42:13 +0800

    完善分页！

--- RxRetrofit/src/main/java/com/sinothk/rxretrofit/interceptor/LogHeaderInterceptor.java
+++ RxRetrofit/src/main/java/com/sinothk/rxretrofit/interceptor/LogHeaderInterceptor.java
@@ -13,7 +13,6 @@ import okhttp3.Headers;
 import okhttp3.Interceptor;
 import okhttp3.MediaType;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 
@@ -76,40 +75,45 @@ public class LogHeaderInterceptor implements Interceptor {
     }
 
     private void printInfo(final Request signedRequest, final HashMap<String, String> headerMap, final Request request, final double time, final String responseBodyStr) {
-        if (BuildConfig.DEBUG) {
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    String TAG = "RxRetrofitLog";
-                    Log.d(TAG, "RxRetrofit > =====================开始=======================");
-                    // url
-                    String url = signedRequest.url().toString();
-                    Log.d(TAG, "RxRetrofit > 地址: " + url);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (BuildConfig.DEBUG) {
+                    try {
+                        String TAG = "RxRetrofitLog";
+                        Log.d(TAG, "RxRetrofit > =====================开始=======================");
+                        // url
+                        String url = signedRequest.url().toString();
+                        Log.d(TAG, "RxRetrofit > 地址: " + url);
 
-                    // 请求方式
-                    String requestType = request.method();
-                    Log.d(TAG, "RxRetrofit > 方式: " + requestType);
+                        // 请求方式
+                        String requestType = request.method();
+                        Log.d(TAG, "RxRetrofit > 方式: " + requestType);
 
-                    if (headerMap != null) {
-                        Log.d(TAG, "RxRetrofit > 头部: " + headerMap.toString());
+                        if (headerMap != null) {
+                            Log.d(TAG, "RxRetrofit > 头部: " + headerMap.toString());
+                        }
+
+                        // 请求信息
+                        Log.d(TAG, "RxRetrofit > 参数：");
+                        Set<String> setList = request.url().queryParameterNames();
+                        for (String paramName : setList) {
+                            String value = request.url().queryParameterValues(paramName).get(0);
+                            Log.d(TAG, "RxRetrofit >     " + paramName + " => " + value);
+                        }
+
+                        Log.d(TAG, "RxRetrofit > 耗时: " + time + "毫秒");
+                        if (needResult) {
+                            Log.d(TAG, "RxRetrofit > 返回: ↓\n" + responseBodyStr);
+                        } else {
+                            Log.d(TAG, "RxRetrofit > 返回: 无需打印！");
+                        }
+                        Log.d(TAG, "RxRetrofit > =====================结束=======================");
+                    } catch (Exception e) {
+                        e.printStackTrace();
                     }
-
-                    // 请求信息
-                    Log.d(TAG, "RxRetrofit > 参数：");
-                    Set<String> setList = request.url().queryParameterNames();
-                    for (String paramName : setList) {
-                        Log.d(TAG, "RxRetrofit >     " + paramName + " => " + request.url().queryParameterValues(paramName));
-                    }
-
-                    Log.d(TAG, "RxRetrofit > 耗时: " + time + "毫秒");
-                    if (needResult) {
-                        Log.d(TAG, "RxRetrofit > 返回: ↓\n" + responseBodyStr);
-                    } else {
-                        Log.d(TAG, "RxRetrofit > 返回: 无需打印！");
-                    }
-                    Log.d(TAG, "RxRetrofit > =====================结束=======================");
                 }
-            }).start();
-        }
+            }
+        }).start();
     }
 }
