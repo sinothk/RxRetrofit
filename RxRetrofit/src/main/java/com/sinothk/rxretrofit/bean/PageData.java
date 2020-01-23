954ec7faba0137f4d68912827d50bbe54930ee23
LiangYT <381518188@qq.com>
2020-01-24 00:47:07 +0800

    调整日志打印！

--- RxRetrofit/src/main/java/com/sinothk/rxretrofit/bean/PageData.java
+++ RxRetrofit/src/main/java/com/sinothk/rxretrofit/bean/PageData.java
@@ -2,7 +2,6 @@ package com.sinothk.rxretrofit.bean;
 
 import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * <pre>
@@ -16,84 +15,16 @@ public class PageData<T> implements Serializable {
 
     //当前页
     private int pageNum;
-    //总页数
-    private int pages;
     //每页的数量
     private int pageSize;
-    //当前页的数量
-    private int size;
 
+    //总页数
+    private int pageTotal;
     //总记录数
     private int total;
+
     //结果集
     private ArrayList<T> list;
-
-    //由于startRow和endRow不常用，这里说个具体的用法
-    //可以在页面中"显示startRow到endRow 共size条数据"
-
-//    //当前页面第一个元素在数据库中的行号
-//    private int startRow;
-//    //当前页面最后一个元素在数据库中的行号
-//    private int endRow;
-
-    // 下一页
-//    private int nextPage;
-//    //第一页
-//    private int firstPage;
-//    //前一页
-//    private int prePage;
-//
-//    //是否为第一页
-//    private boolean isFirstPage = false;
-//    //是否为最后一页
-//    private boolean isLastPage = false;
-    //是否有前一页
-    private boolean hasPreviousPage = false;
-    //是否有下一页
-    private boolean hasNextPage = false;
-//    //导航页码数
-//    private int navigatePages;
-//    //所有导航页号
-//    private int[] navigatepageNums;
-//
-//    private int navigateFirstPage;
-//    private int navigateLastPage;
-
-    public String toString() {
-        StringBuilder sb = new StringBuilder("PageInfo{");
-        sb.append("pageNum=").append(this.pageNum);
-        sb.append(", pageSize=").append(this.pageSize);
-        sb.append(", size=").append(this.size);
-//        sb.append(", startRow=").append(this.startRow);
-//        sb.append(", endRow=").append(this.endRow);
-        sb.append(", total=").append(this.total);
-        sb.append(", pages=").append(this.pages);
-        sb.append(", list=").append(this.list);
-//        sb.append(", prePage=").append(this.prePage);
-//        sb.append(", nextPage=").append(this.nextPage);
-//        sb.append(", isFirstPage=").append(this.isFirstPage);
-//        sb.append(", isLastPage=").append(this.isLastPage);
-        sb.append(", hasPreviousPage=").append(this.hasPreviousPage);
-        sb.append(", hasNextPage=").append(this.hasNextPage);
-//        sb.append(", navigatePages=").append(this.navigatePages);
-//        sb.append(", navigateFirstPage=").append(this.navigateFirstPage);
-//        sb.append(", navigateLastPage=").append(this.navigateLastPage);
-//        sb.append(", navigatepageNums=");
-//        if (this.navigatepageNums == null) {
-//            sb.append("null");
-//        } else {
-//            sb.append('[');
-//
-//            for (int i = 0; i < this.navigatepageNums.length; ++i) {
-//                sb.append(i == 0 ? "" : ", ").append(this.navigatepageNums[i]);
-//            }
-//
-//            sb.append(']');
-//        }
-
-        sb.append('}');
-        return sb.toString();
-    }
 
     public int getPageNum() {
         return pageNum;
@@ -111,117 +42,13 @@ public class PageData<T> implements Serializable {
         this.pageSize = pageSize;
     }
 
-    public int getSize() {
-        return size;
+    public int getPageTotal() {
+        return pageTotal;
     }
 
-    public void setSize(int size) {
-        this.size = size;
+    public void setPageTotal(int pageTotal) {
+        this.pageTotal = pageTotal;
     }
-
-//    public int getStartRow() {
-//        return startRow;
-//    }
-//
-//    public void setStartRow(int startRow) {
-//        this.startRow = startRow;
-//    }
-//
-//    public int getEndRow() {
-//        return endRow;
-//    }
-//
-//    public void setEndRow(int endRow) {
-//        this.endRow = endRow;
-//    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-//    public int getPrePage() {
-//        return prePage;
-//    }
-//
-//    public void setPrePage(int prePage) {
-//        this.prePage = prePage;
-//    }
-//
-//    public int getNextPage() {
-//        return nextPage;
-//    }
-//
-//    public void setNextPage(int nextPage) {
-//        this.nextPage = nextPage;
-//    }
-
-//    public boolean isFirstPage() {
-//        return isFirstPage;
-//    }
-//
-//    public void setFirstPage(boolean firstPage) {
-//        isFirstPage = firstPage;
-//    }
-//
-//    public boolean isLastPage() {
-//        return isLastPage;
-//    }
-//
-//    public void setLastPage(boolean lastPage) {
-//        isLastPage = lastPage;
-//    }
-
-    public boolean isHasPreviousPage() {
-        return hasPreviousPage;
-    }
-
-    public void setHasPreviousPage(boolean hasPreviousPage) {
-        this.hasPreviousPage = hasPreviousPage;
-    }
-
-    public boolean isHasNextPage() {
-        return hasNextPage;
-    }
-
-    public void setHasNextPage(boolean hasNextPage) {
-        this.hasNextPage = hasNextPage;
-    }
-
-//    public int getNavigatePages() {
-//        return navigatePages;
-//    }
-//
-//    public void setNavigatePages(int navigatePages) {
-//        this.navigatePages = navigatePages;
-//    }
-//
-//    public int[] getNavigatepageNums() {
-//        return navigatepageNums;
-//    }
-//
-//    public void setNavigatepageNums(int[] navigatepageNums) {
-//        this.navigatepageNums = navigatepageNums;
-//    }
-//
-//    public int getNavigateFirstPage() {
-//        return navigateFirstPage;
-//    }
-//
-//    public void setNavigateFirstPage(int navigateFirstPage) {
-//        this.navigateFirstPage = navigateFirstPage;
-//    }
-//
-//    public int getNavigateLastPage() {
-//        return navigateLastPage;
-//    }
-//
-//    public void setNavigateLastPage(int navigateLastPage) {
-//        this.navigateLastPage = navigateLastPage;
-//    }
 
     public int getTotal() {
         return total;
